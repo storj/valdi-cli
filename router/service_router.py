5c82a2e6c1cb41fae1cf7eb67891b55b02ab3850
Mark Harvilla <harvilm@gmail.com>
2024-01-29 14:48:22 -0800

    Connect argument parser with service router and volume manager

--- router/service_router.py
+++ router/service_router.py
@@ -6,23 +6,23 @@ from config.settings import Config
 
 class ServiceRouter:
     @staticmethod
-    def route_to_service(service, cmd, *params):
-        if service == Config.Service.INIT.value:
+    def route_to_service(args):
+        if args.service == Config.Service.INIT.value:
             # initialize CLI tool
             Initializer.initialize()
             print('Successfully initialized')
         else:
             auth = Authenticator()
-            if service == Config.Service.VOLUME.value:
+            if args.service == Config.Service.VOLUME.value:
                 # handle VOLUME commands
                 volume_manager = VolumeManager(auth)
-                if cmd == Config.Command.MOUNT.value:
-                    print(f'Mount volume with params {params[0]}')
-                elif cmd == Config.Command.UNMOUNT.value:
-                    print('Unmount volume')
+                if args.cmd == Config.Command.MOUNT.value:
+                    volume_manager.mount_volume(args.volume_name, args.mountpoint)
+                elif args.cmd == Config.Command.UNMOUNT.value:
+                    volume_manager.unmount_volume(args.mointpoint)
                 else:
-                    raise RuntimeError(f'Unrecognized command for {service} service')
-            elif service == Config.Service.VM.value:
+                    raise RuntimeError(f'Unrecognized command for {args.service} service')
+            elif args.service == Config.Service.VM.value:
                 # handle VM commands
                 raise NotImplementedError('VM services not yet implemented')
             else:
--- valdi.py
+++ valdi.py
@@ -4,17 +4,34 @@ from router.service_router import ServiceRouter
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='valdi')
+    subparsers = parser.add_subparsers(dest='service', required=True, metavar='service')
 
-    service_choices = [service.value for service in Config.Service]
-    command_choices = [command.value for command in Config.Command]
-
-    parser.add_argument('service', type=str, choices=service_choices)
-    parser.add_argument('cmd', type=str, choices=command_choices)
-    parser.add_argument('params', nargs='*', help='Additional parameters for this command')
+    for service in Config.Service:
+        if service.value == Config.Service.VOLUME.value:
+            service_parser = subparsers.add_parser(service.value, help='Manage detachable volumes')
+            service_subparsers = service_parser.add_subparsers(dest='cmd', required=True, metavar='command')
+            for cmd in Config.VolumeCommand:
+                if cmd.value == Config.VolumeCommand.MOUNT.value:
+                    cmd_parser = service_subparsers.add_parser(
+                        cmd.value,
+                        help='Mount a detachable volume'
+                    )
+                    cmd_parser.add_argument('volume_name', help='Name of volume to mount')
+                    cmd_parser.add_argument('mountpoint', help='Mount point for your volume')
+                elif cmd.value == Config.VolumeCommand.UNMOUNT.value:
+                    cmd_parser = service_subparsers.add_parser(
+                        cmd.value,
+                        help='Unmount a detachable volume'
+                    )
+                    cmd_parser.add_argument('mountpoint', help='Mount point of volume to unmount')
+        elif service.value == 'vm':
+            _ = subparsers.add_parser(service.value, help='Manage virtual machines')
+        elif service.value == 'init':
+            _ = subparsers.add_parser(service.value, help='Initialize VALDI CLI')
 
     args = parser.parse_args()
-    ServiceRouter.route_to_service(args.service, args.cmd, *args.params)
+    ServiceRouter.route_to_service(args)
 
 
 if __name__ == "__main__":
