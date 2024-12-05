7d853b604a839f08e0e8c90169abb60f843f2d04
JT Olio <jt@olio.lol>
2024-12-05 11:22:00 -0500

    Format all python code with 'black' code formatting
    This reduces future code merge conflicts by eliminating
    differences in code formatting style, similar to Go's
    gofmt tool.
    
    pip install black

--- valdi/cli/authenticator.py
+++ valdi/cli/authenticator.py
@@ -5,6 +5,7 @@ import getpass
 import requests
 from pathlib import Path
 from datetime import datetime
+
 from valdi.config.settings import Config
 
 
@@ -13,7 +14,9 @@ class Authenticator:
         self.user_email = None
         self.refresh_token = None
         self._access_token = None
-        self.credentials_filepath = Path(__file__).parent.parent.parent / Config.VALDI_CREDENTIALS_FILE
+        self.credentials_filepath = (
+            Path(__file__).parent.parent.parent / Config.VALDI_CREDENTIALS_FILE
+        )
         if os.path.isfile(self.credentials_filepath):
             self._read_credentials()
             if self._is_token_expired(self.refresh_token):
@@ -32,39 +35,39 @@ class Authenticator:
 
     def _get_user_info(self):
         response = requests.get(
-            url=f'{Config.VALDI_BASE_URL}/account',
+            url=f"{Config.VALDI_BASE_URL}/account",
             headers={
-                'Content-Type': 'application/json',
-                'Authorization': f'Bearer {self.access_token}'
-            }
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.access_token}",
+            },
         )
         response.raise_for_status()
         return json.loads(response.text)
 
     def _renew_credentials(self, verbose=False):
         if verbose:
-            print('Your credentials have expired, you must reauthenticate.')
+            print("Your credentials have expired, you must reauthenticate.")
         hidden_dir = self.credentials_filepath.parent
         os.makedirs(hidden_dir, exist_ok=True)
 
-        self.user_email = input('Account email: ')
-        account_pass = getpass.getpass('Password: ')
+        self.user_email = input("Account email: ")
+        account_pass = getpass.getpass("Password: ")
 
         response = requests.post(
-            url=f'{Config.VALDI_BASE_URL}/account/login',
-            headers={'Content-Type': 'application/json'},
-            data=json.dumps({'email': self.user_email, 'password': account_pass})
+            url=f"{Config.VALDI_BASE_URL}/account/login",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps({"email": self.user_email, "password": account_pass}),
         )
         response.raise_for_status()
         json_response = json.loads(response.text)
-        self.refresh_token = json_response['refresh_token']
-        self._access_token = json_response['access_token']
+        self.refresh_token = json_response["refresh_token"]
+        self._access_token = json_response["access_token"]
 
-        with open(self.credentials_filepath, 'w') as f:
+        with open(self.credentials_filepath, "w") as f:
             f.write(self.refresh_token)
 
     def _read_credentials(self):
-        with open(self.credentials_filepath, 'r') as f:
+        with open(self.credentials_filepath, "r") as f:
             self.refresh_token = f.read()
 
     def _renew_access_token(self):
@@ -72,18 +75,18 @@ class Authenticator:
             self._renew_credentials(verbose=True)
         else:
             response = requests.post(
-                url=f'{Config.VALDI_BASE_URL}/account/refresh_token',
-                headers={'Content-Type': 'application/json'},
-                data=json.dumps({'token': self.refresh_token})
+                url=f"{Config.VALDI_BASE_URL}/account/refresh_token",
+                headers={"Content-Type": "application/json"},
+                data=json.dumps({"token": self.refresh_token}),
             )
             response.raise_for_status()
             json_response = json.loads(response.text)
-            self._access_token = json_response['access_token']
+            self._access_token = json_response["access_token"]
 
     @staticmethod
     def _is_token_expired(token):
-        decoded_token = jwt.decode(token, options={'verify_signature': False})
-        token_expiry = datetime.strptime(str(decoded_token['exp']), '%Y%m%d%H%M%S')
+        decoded_token = jwt.decode(token, options={"verify_signature": False})
+        token_expiry = datetime.strptime(str(decoded_token["exp"]), "%Y%m%d%H%M%S")
         if datetime.utcnow() > token_expiry:
             return True
         else:
--- valdi/cli/volume_manager.py
+++ valdi/cli/volume_manager.py
@@ -3,6 +3,7 @@ import sys
 import getpass
 import subprocess
 from pathlib import Path
+
 from valdi.config.settings import Config
 from valdi.cli.authenticator import Authenticator
 
@@ -15,9 +16,9 @@ class VolumeManager:
     def _volume_access_credentials_are_available(volume_name):
         goofys_credentials_file = Path(Config.GOOFYS_CREDENTIALS_FILE).expanduser()
         if goofys_credentials_file.exists():
-            with open(goofys_credentials_file, 'r') as f:
+            with open(goofys_credentials_file, "r") as f:
                 for line in f:
-                    if line.strip() == f'[{volume_name}]':
+                    if line.strip() == f"[{volume_name}]":
                         return True
             return False
         else:
@@ -26,58 +27,58 @@ class VolumeManager:
 
     @staticmethod
     def _store_volume_access_credentials(volume_name):
-        access_key = input('Enter access key: ')
-        secret_key = getpass.getpass('Enter secret access key: ')
+        access_key = input("Enter access key: ")
+        secret_key = getpass.getpass("Enter secret access key: ")
         goofys_credentials_file = Path(Config.GOOFYS_CREDENTIALS_FILE).expanduser()
-        with open(goofys_credentials_file, 'a') as f:
-            f.write(f'[{volume_name}]\naws_access_key_id = {access_key}\naws_secret_access_key = {secret_key}\n')
+        with open(goofys_credentials_file, "a") as f:
+            f.write(
+                f"[{volume_name}]\naws_access_key_id = {access_key}\naws_secret_access_key = {secret_key}\n"
+            )
 
     def mount_volume(self, prefix_to_mount, mountpoint):
         present_working_dir = Path(__file__)
         goofys_exe = present_working_dir.parent.parent.parent / Config.GOOFYS_EXE_FILE
         mount_point = Path(mountpoint).expanduser()
         if not mount_point.is_absolute():
-            print('mountpoint must be a full path')
+            print("mountpoint must be a full path")
             sys.exit(1)
         os.makedirs(mount_point, exist_ok=True)
 
         if not self._volume_access_credentials_are_available(prefix_to_mount):
             self._store_volume_access_credentials(prefix_to_mount)
 
-        original_aws_profile = os.environ.get('AWS_PROFILE')
-        os.environ['AWS_PROFILE'] = prefix_to_mount
+        original_aws_profile = os.environ.get("AWS_PROFILE")
+        os.environ["AWS_PROFILE"] = prefix_to_mount
         try:
-            subprocess.run([
-                goofys_exe,
-                '--endpoint',
-                Config.VALDI_GATEWAY_URL,
-                f'{Config.VALDI_GLOBAL_ROOT}:{self.authenticator.user_info["user_id"]}/{prefix_to_mount}',
-                mount_point
-            ],
-                check=True
+            subprocess.run(
+                [
+                    goofys_exe,
+                    "--endpoint",
+                    Config.VALDI_GATEWAY_URL,
+                    f'{Config.VALDI_GLOBAL_ROOT}:{self.authenticator.user_info["user_id"]}/{prefix_to_mount}',
+                    mount_point,
+                ],
+                check=True,
             )
         except subprocess.CalledProcessError as e:
-            print(f'Error mounting volume: {e.stderr}')
+            print(f"Error mounting volume: {e.stderr}")
             sys.exit(1)
         finally:
             if original_aws_profile is not None:
-                os.environ['AWS_PROFILE'] = original_aws_profile
+                os.environ["AWS_PROFILE"] = original_aws_profile
             else:
-                os.environ.pop('AWS_PROFILE', None)
+                os.environ.pop("AWS_PROFILE", None)
 
     @staticmethod
     def unmount_volume(mountpoint):
         if not Path(mountpoint).is_absolute():
-            print('mountpoint must be a full path')
+            print("mountpoint must be a full path")
             sys.exit(1)
 
         try:
-            subprocess.run([
-                'umount',
-                mountpoint
-            ],
-                check=True
-            )
+            subprocess.run(["umount", mountpoint], check=True)
         except subprocess.CalledProcessError as e:
-            print(f'Error unmounting volume{f": {e.stderr}" if e.stderr is not None else ""}')
+            print(
+                f'Error unmounting volume{f": {e.stderr}" if e.stderr is not None else ""}'
+            )
             sys.exit(1)
