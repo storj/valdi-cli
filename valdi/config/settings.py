8146c6574b0e6711470ccea9ed3ddf1d34f5bd01
JT Olio <jt@olio.lol>
2024-12-05 15:42:43 -0500

    Support cunoFS in addition to Goofys
    This adds a new volume manager that knows how to configure
    cunoFS and use cunoFS for mounting volumes.
    
    There are some major outstanding issues before this can actually
    be enabled by default:
    
    1) We need to figure out how to distribute cunoFS officially. The
       main distributions are install scripts with an EULA. Should
       we roll the cunoFS EULA into the Valdi EULA and assume it's
       already approved, or should we make the customer accept it in
       terminal?
    2) We need to figure out license distribution - this should
       probably expire and renew along with the Valdi API JWT.
    
    However, if you do have an available tarball (see
    Config.CunoFS.URL) that includes an installed cunoFS distribution
    with a license, this otherwise works great.

--- valdi/config/settings.py
+++ valdi/config/settings.py
@@ -8,3 +8,13 @@ class Config:
         URL = "https://github.com/kahing/goofys/releases/latest/download/goofys"
         CREDENTIALS_FILE = "~/.aws/credentials"
         EXE_FILE = "goofys"
+
+    class CunoFS:
+        URL = (
+            "https://link.storjshare.io/raw/<redacted>/"
+            "cuno-dist/cuno-dist-v1.2.6.tar.gz"
+        )
+        BASE_FOLDER = "cunofs"
+        VER_FILE = "1.2.5"
+
+    BACKEND = Goofys
